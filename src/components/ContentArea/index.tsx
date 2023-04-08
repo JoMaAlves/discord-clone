11969b9e5f241be27590a02b0c9613b083ce4839
Joao Marcelo Alves <joao.alves@liferay.com>
2023-04-08 01:42:04 -0300

    Created Basic Components and Installed some Libs

---
+++ src/components/ContentArea/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function ContentArea({}: Props) {
+  return <div className="col">ContentArea</div>;
+}
---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function SideBar({}: Props) {
+  return <div className="side-bar-container no-wrap">SideBar</div>;
+}
